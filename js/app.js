b4d28b9b1339b02e4dcb7a91d56dd18c7280c15b
rafnixg <rafnixg@gmail.com>
2015-08-14 01:09:12 -0430

    Refactorizacion

--- js/app.js
+++ js/app.js
@@ -4,22 +4,19 @@
 
 $(document).ready(function(){
   	
-
-    
-
-
 	//Funcion para procesar los archivos CSV - Lee Linea a Linea
 	//Retorna una array.
-	function processData(allText) {
-		var allTextLines = allText.split(/\r\n|\n/);
-		var lines = [];
-		$.each(allTextLines, function(index, value) {
-			lines.push(parseFloat(value));
+	function processData(textoCompleto) {
+		var texto = textoCompleto.split(/\r\n|\n/);
+		var lineas = [];
+		$.each(texto, function(index, value) {
+			lineas.push(parseFloat(value));
     	});
 
-		return lines;
+		return lineas;
 	};
-
+	//Funcion para hacer los calculos del valor rms,
+	//valor promedio, y valor promedio rectificado.
 	function calcular(datos){
 		var promedio = 0;
         var promedio_rect = 0;
@@ -45,6 +42,7 @@ $(document).ready(function(){
         vector[3] = valor_rms;
         return vector;
 	};
+	//calcula el modul a un array
 	function modulo(datos){
 		for (var i = 0; i < datos.length; i++) {
 			datos[i]= Math.abs( datos[i]);
@@ -53,20 +51,16 @@ $(document).ready(function(){
 
 	};
 	
-
+	//Cargando el archivo con el metodo GET
 	$.ajax({
         type: "GET",
         url: "./archivos/voltaje.csv",
         dataType: "text",
         success: function(data) {
-            var v_data = [];
-			var rv_data = [];
 			var vectorV = [];
             var data_procesadaV = processData(data, false);
             vectorV = calcular(data_procesadaV);
-
-            
-
+            //Imprimo los datos en el HTML
             $('#promedioV').html("Valor Promedio: " + vectorV[1] + " [V]");
             $('#rmsV').html("Valor RMS: " + vectorV[2] + " [V]");
             $('#promedio_rect_V').html("Valor Promedio Rectificado: " + vectorV[3] + " [V]");
@@ -122,21 +116,20 @@ $(document).ready(function(){
 		            data: modulo(data_procesadaV)
 		        }]
 		    });
+            //Cargando el archivo de corriente con metodo GET
 			$.ajax({
 		        type: "GET",
 		        url: "./archivos/corriente4.csv",
 		        dataType: "text",
 		        success: function(data) {
-		            var v_data = [];
-					var rv_data = [];
 					var vectorC = [];
 		            var data_procesadaC = processData(data, false);
 		            vectorC = calcular(data_procesadaC);
-
+		            //Imprimiendo datos al HTML
 		            $('#promedioC').html("Valor Promedio: " + vectorC[1] + " [A]");
 		            $('#rmsC').html("Valor RMS: " + vectorC[2] + " [A]");
 		            $('#promedio_rect_C').html("Valor Promedio Rectificado: " + vectorC[3] + " [A]");
-		           
+		           	//Graficando Corriente
 		           	$('#gCorriente').highcharts({
 				        title: {
 				            text: 'Grafica de Corriente',
@@ -162,7 +155,7 @@ $(document).ready(function(){
 				            data: data_procesadaC
 				        }]
 				    });
-
+		           	//Graficando Corriente Rectificada
 				    $('#gCorrienteR').highcharts({
 				        title: {
 				            text: 'Grafica de Corriente Rectificada',
@@ -188,7 +181,7 @@ $(document).ready(function(){
 				            data: modulo(data_procesadaC)
 				        }]
 				    });
-
+				    //Calculos de potencia
 					var potencia_prom = 0;
 					var potencia_inst = [];
             		for (var i = 0; i < data_procesadaV.length; i++) {
@@ -198,10 +191,10 @@ $(document).ready(function(){
             		};
             		potencia_prom= potencia_prom/data_procesadaV.length;
             		var potencia_aparente = vectorC[2]*vectorV[2];
-
+            		//Imprimiendo datos al HTML
             		$('#potenciaProm').html("Potencia Promedio por Ciclo: " + potencia_prom + " [W]");
 		            $('#potenciaAparente').html("Potencia Aparente: " + potencia_aparente + " [W]");
-		            
+		            //Graficando potencia
         			$('#gPotencia').highcharts({
 				        title: {
 				            text: 'Grafica de Potencia Instantanea',
